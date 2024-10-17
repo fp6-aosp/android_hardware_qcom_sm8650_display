6e4c9bd99baa118d18a81f5b8f1ea4b5291d236b
Dhruv Kaushik <quic_dhrukaus@quicinc.com>
2024-10-17 21:50:39 +0530

    composer: Add overflow check on MaxDisplays counts
    If panel node is invalid, max_builtin/max_pluggable can get
    initialized to 0 which can cause an overflow error.
    
    Change-Id: Id91ce7e9827b47799157919533f85e387b3bd6ba

--- composer/hwc_session.cpp
+++ composer/hwc_session.cpp
@@ -433,9 +433,15 @@ void HWCSession::InitSupportedDisplaySlots() {
 
   if (kPluggable == hw_disp_info.type) {
     // If primary is a pluggable display, we have already used one pluggable display interface.
-    max_pluggable--;
+    // max_pluggable/builtin can both be initialized to 0 in case of invalid panel node
+    // check to avoid overflow
+    if (max_pluggable) {
+      max_pluggable--;
+    }
   } else {
-    max_builtin--;
+    if (max_builtin) {
+      max_builtin--;
+    }
   }
 
   // Init slots in accordance to h/w capability.
